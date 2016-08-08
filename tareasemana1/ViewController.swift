7e9661e2aa602cadc16c79a6443926029fa17dea
CAAPPLEPC <caapplepc@caapplepcs-mac.local>
2016-08-07 23:01:01 -0400

    semana 2

--- tareasemana1/ViewController.swift
+++ tareasemana1/ViewController.swift
@@ -13,15 +13,35 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var isbn: UITextField!
     
-    @IBOutlet weak var resultadoBusqueda: UITextView!
     
+    @IBOutlet weak var tituloDeLibro: UILabel!
+    
+    @IBOutlet weak var losAutores: UILabel!
+    
+    @IBOutlet weak var portadaDeLibro: UILabel!
     
     func sincrono() {
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn.text!)"
         let url = NSURL(string: urls)
-        let datos:NSData? = NSData(contentsOfURL: url!)
-        let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding )
-        resultadoBusqueda.text = String(texto)
+        let datos = NSData(contentsOfURL: url!)
+        
+        do {
+           let json = try NSJSONSerialization.JSONObjectWithData(datos!,options: NSJSONReadingOptions.MutableLeaves)
+            let dico1 = json as! NSDictionary
+            let dico2 = dico1["ISBN:978-84-376-0494-7"] as! NSDictionary
+            let dico3 = dico2["authors"] as! NSArray
+            self.tituloDeLibro.text = dico2["title"] as! NSString as String
+            self.losAutores.text = dico3[0]["name"] as! NSString as String
+            self.portadaDeLibro.text = dico2["by_statement"] as! NSString as String
+            
+          
+        
+        
+        }
+        catch _ {
+            
+        }
+    
     }
    
     override func viewDidLoad() {
@@ -40,7 +60,9 @@ class ViewController: UIViewController {
     
     @IBAction func clear(sender: AnyObject) {
         isbn.text = " "
-        resultadoBusqueda.text = " "
+        tituloDeLibro.text = " "
+        losAutores.text = " "
+        portadaDeLibro.text = " "
     }
 
 }
